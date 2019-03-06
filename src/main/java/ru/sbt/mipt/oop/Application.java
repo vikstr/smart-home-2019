59fbb9204e7a9c6d34e055cd459677f166480c64
Victoria Strenadko <31935971+vikstr@users.noreply.github.com>
2019-03-06 18:56:23 +0300

    Refactored SOLID

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -5,19 +5,21 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static ru.sbt.mipt.oop.SensorEventType.*;
 
 public class Application {
 
-    private static Smarthomeloader smarthomeloader = new GsonSmartHomeLoader();
+    private static LoaderSmartHome smarthomeloader = new GsonSmartHomeLoader();
 
-    public static void loadSmartHome(LoadSmartHome smarthomeloader) {
+    public static void setSmarthomeloader(LoaderSmartHome smarthomeloader) {
         Application.smarthomeloader = smarthomeloader;
     }
 
     public static void main(String... args) throws IOException {
-        SmartHome smarthome = smarthomeloader.loadsmarthome();
+        SmartHome smarthome = smarthomeloader.loadSmartHome();
         startEvents(smarthome);
     }
     private static void startEvents(SmartHome smartHome) {
@@ -25,16 +27,16 @@ public class Application {
         Collection<EventProcessor> eventProcessorCollection = configureEventProcessors();
         while (sensorEvent != null) {
             System.out.println("Got event: " + sensorEvent);
-            for (EventProcessor eventProcessorCollection : event) {
-                event.processEvent(smartHome, sensorEvent);
+            for (EventProcessor eventProcess : eventProcessorCollection) {
+                eventProcess.processEvent(smartHome, sensorEvent);
             }
             sensorEvent = CreateRandomEvent.getNextSensorEvent();
         }
     }
     private static Collection<EventProcessor> configureEventProcessors() {
         Collection<EventProcessor> eventProcessorCollection = new ArrayList<>();
-        eventProcessorCollection.add(new LightsEvents());
-        eventProcessorCollection.add(new DoorEventProcessor());
+        eventProcessorCollection.add(new LightsEvent());
+        eventProcessorCollection.add(new DoorEvent());
         eventProcessorCollection.add(new MainDoorEvent());
         return eventProcessorCollection;
     }
