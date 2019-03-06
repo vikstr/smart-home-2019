238f578036d92ef05d40231c322f0f583249ffaa
Victoria Strenadko <31935971+vikstr@users.noreply.github.com>
2019-03-06 09:33:15 +0300

    Refactored(SOLID)

---
+++ src/main/java/ru/sbt/mipt/oop/CreateRandomEvent.java
@@ -0,0 +1,10 @@
+package ru.sbt.mipt.oop;
+
+public class CreateRandomEvent {
+    public static SensorEvent getNextSensorEvent() {
+        if (Math.random() < 0.05) return null; // null means end of event stream
+        SensorEventType sensorEventType = SensorEventType.values()[(int) (4 * Math.random())];
+        String objectId = "" + ((int) (10 * Math.random()));
+        return new SensorEvent(sensorEventType, objectId);
+    }
+}
