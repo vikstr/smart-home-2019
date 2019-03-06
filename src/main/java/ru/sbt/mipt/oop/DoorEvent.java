7d616657749e25e3efb3221f208788725b2aaade
Victoria Strenadko <31935971+vikstr@users.noreply.github.com>
2019-03-06 17:12:16 +0300

    Refactored(SOLID)

---
+++ src/main/java/ru/sbt/mipt/oop/DoorEvent.java
@@ -0,0 +1,32 @@
+package ru.sbt.mipt.oop;
+
+import static ru.sbt.mipt.oop.SensorEventType.DOOR_CLOSED;
+import static ru.sbt.mipt.oop.SensorEventType.DOOR_OPEN;
+
+public class DoorEvent implements EventProcessor {
+
+    public void processEvent(SmartHome smartHome, SensorEvent event) {
+        if (!isDoorEvent(event)) return;
+        // событие от двери
+        for (Room room : smartHome.getRooms()) {
+            for (Door door : room.getDoors()) {
+                if (door.getId().equals(event.getObjectId())) {
+                    if (event.getType() == DOOR_OPEN) {
+                        changeDoorState(room, door, true, " was opened.");
+                    } else {
+                        changeDoorState(room, door, false, " was closed.");
+                    }
+                }
+            }
+        }
+    }
+
+    private void changeDoorState(Room room, Door door, boolean opened, String s) {
+        door.setOpen(opened);
+        System.out.println("Door " + door.getId() + " in room " + room.getName() + s);
+    }
+
+    private boolean isDoorEvent(SensorEvent event) {
+        return event.getType() == DOOR_OPEN || event.getType() == DOOR_CLOSED;
+    }
+}
