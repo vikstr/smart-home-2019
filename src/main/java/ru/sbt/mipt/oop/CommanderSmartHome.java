9ab4349f59e16749e9204b513b1c853925b8fd06
Victoria Strenadko <31935971+vikstr@users.noreply.github.com>
2019-03-06 19:11:22 +0300

    REFACTORED SOLID

---
+++ src/main/java/ru/sbt/mipt/oop/CommanderSmartHome.java
@@ -0,0 +1,7 @@
+package ru.sbt.mipt.oop;
+
+public class CommanderSmartHome {
+    public static void giveCommand(SensorCommand command) {
+        System.out.println("Pretend we're sending command " + command);
+    }
+}
